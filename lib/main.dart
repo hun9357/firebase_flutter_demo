946bb69e94c059b8431f80c299488ce646706800
hun9357 <hun935701@gmail.com>
2023-01-02 11:19:19 -0600

    FB auth and FB store connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:login_demo/screens/homepage.dart';
 import 'package:login_demo/screens/login.dart';
+import 'package:login_demo/screens/register.dart';
 import 'firebase_options.dart';
 
 Future main() async {
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:login_demo/screens/homepage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:login_demo/screens/login.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -8,9 +12,26 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPage extends State<RegisterPage> {
-  navigateToHome() {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => const HomePage()));
+  Future navigateToAdd(uid) async {
+    print(uid);
+    await Navigator.push(
+        context, MaterialPageRoute(builder: (context) => additionalPage(uid)));
+  }
+
+  final emailController = TextEditingController();
+  final pwController = TextEditingController();
+  var uid;
+
+  Future createUser() async {
+    await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(
+            email: emailController.text, password: pwController.text)
+        .then((value) async => uid = value.user?.uid);
+    await FirebaseFirestore.instance
+        .collection('User')
+        .doc(uid)
+        .set({'first_name': 'random', 'last_name': 'random'});
+    return uid;
   }
 
   @override
@@ -38,42 +59,133 @@ class _RegisterPage extends State<RegisterPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const Text("Create your password",
-                  style: TextStyle(fontSize: 25)),
-              const SizedBox(
+              Text("Create your password", style: TextStyle(fontSize: 25)),
+              SizedBox(
                 height: 55,
               ),
-              const Text('Your password must be at least 6 characters'),
-              const SizedBox(
+              Text('Your password must be at least 6 characters'),
+              SizedBox(
                 height: 55,
               ),
-              const TextField(
+              TextField(
+                controller: emailController,
                 decoration: InputDecoration(
                   icon: Icon(Icons.email),
                   hintText: 'enter your e-mail address',
                 ),
               ),
-              const TextField(
+              TextField(
+                controller: pwController,
                 decoration: InputDecoration(
                   icon: Icon(Icons.lock),
                   hintText: 'Enter your password',
                 ),
               ),
-              const TextField(
+              TextField(
                 decoration: InputDecoration(
                   icon: Icon(Icons.lock),
                   hintText: 'Confirm your password',
                 ),
               ),
-              const Text('By signing up you agree to our Term'),
-              const Text('and conditions of use'),
+              Text('By signing up you agree to our Term'),
+              Text('and conditions of use'),
               ElevatedButton(
-                  onPressed: () {
-                    navigateToHome();
+                  onPressed: () async {
+                    uid = await createUser();
+                    await navigateToAdd(uid);
                   },
-                  child: const Text('Continue ->'))
+                  child: Text('Continue ->'))
             ],
           ),
         ));
   }
+}
+
+class additionalPage extends StatefulWidget {
+  var uid;
+  additionalPage(this.uid);
+  @override
+  State<additionalPage> createState() => _additionalPage(this.uid);
+}
+
+class _additionalPage extends State<additionalPage> {
+  var uid;
+  _additionalPage(this.uid);
+  navigateToHome() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => HomePage()));
+  }
+
+  Future setName(uid) async {
+    print(uid);
+    var data = await FirebaseFirestore.instance
+        .collection("User")
+        .doc(uid)
+        .update(
+            {'first_name': fnController.text, 'last_name': lnController.text});
+  }
+
+  final fnController = TextEditingController();
+  final lnController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+              onPressed: () {
+                setState(() {
+                  Navigator.pop(context);
+                });
+              },
+              icon: const Icon(
+                Icons.arrow_back_ios_new_outlined,
+                color: Colors.blue,
+              )),
+          title: const Text(
+            'Personal Information',
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Enter your informations", style: TextStyle(fontSize: 25)),
+              SizedBox(
+                height: 55,
+              ),
+              TextField(
+                controller: fnController,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.person),
+                  hintText: 'enter your first name',
+                ),
+              ),
+              TextField(
+                controller: lnController,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.person),
+                  hintText: 'Enter your last name',
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.lock),
+                  hintText: 'Confirm your password',
+                ),
+              ),
+              Text('By signing up you agree to our Term'),
+              Text('and conditions of use'),
+              ElevatedButton(
+                  onPressed: () {
+                    setName(uid);
+                    navigateToHome();
+                  },
+                  child: const Text('Submit'))
+            ],
+          ),
+        ),
+      );
 }
